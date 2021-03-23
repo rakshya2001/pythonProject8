ff1658ec9a3247e3f7f1297d2642f1bd23528427
rakshya2001 <bhattarakshya6@gmail.com>
2021-03-23 11:59:16 +0545

    exercise 2

---
+++ lab exercise 2.py
@@ -0,0 +1,6 @@
+# check whether 5 is in list of first natural numbers or not.Hint: List=>[1,2,3,4,5]
+List=[1,2,3,4,5]
+if 5 in List:
+    print(f'5 is a natural number')
+else:
+    print(f'5 is not in list')
