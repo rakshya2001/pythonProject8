89e2876ac5fb5d5bae9c7d7b3f70b42a4ee24ff4
rakshya2001 <bhattarakshya6@gmail.com>
2021-05-12 18:17:15 +0545

    ...

--- reg.py
+++ reg.py
@@ -10,3 +10,8 @@ class user_registration:
         self.root = Tk()
         self.root.title("User Registration Portal")  # To create a window for the registration
         self.root.geometry("1650x1350+0+0")
+
+        ####=====Image======####
+        self.img = 'school.png'  # inserting image
+        self.bg = ImageTk.PhotoImage(Image.open(self.img))
+        self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
